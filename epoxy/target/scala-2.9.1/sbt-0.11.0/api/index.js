426064e2d0cb0acf70454c2c9ec940e22ec84323
Jacques Lemieux <jacques@lemieux.cc>
2011-11-29 13:14:36 -0500

    cleanup

--- epoxy/target/scala-2.9.1/sbt-0.11.0/api/index.js
+++
@@ -1,1 +0,0 @@
-Index.PACKAGES = {"net" : [], "net.strong_links" : [], "net.strong_links.epoxy" : [{"class" : "net\/strong_links\/epoxy\/TemplateParser.html", "name" : "net.strong_links.epoxy.TemplateParser"}, {"object" : "net\/strong_links\/epoxy\/TemplComp$.html", "class" : "net\/strong_links\/epoxy\/TemplComp.html", "name" : "net.strong_links.epoxy.TemplComp"}]};
