0f03214e6a62c8b58adbcc4ff9d9ba007bdebddd
Maxime Lévesque <maxime.levesque@gmail.com>
2011-11-30 14:55:33 -0500

    Initialisation improvement

--- epoxy/src/main/scala/net/strong_links/epoxy/Epoxy.scala
+++ epoxy/src/main/scala/net/strong_links/epoxy/Epoxy.scala
@@ -48,11 +48,10 @@ object Epoxy {
   def init = {      
       
     val epoxyTask = (TaskKey[Seq[File]]("epoxy") in Compile) <<= defineEpoxyTask(false)
-    val emptyFileSeqTask = (sourceDirectories) map(d => Nil : Seq[File])
     
     Seq(
-      //(epoxyTemplateRoots) <++= emptyFileSeqTask,
-      //(epoxyResourceRoots) <++= emptyFileSeqTask,      
+      (epoxyTemplateRoots) <<= sourceDirectory.map(src =>  Nil: Seq[File]),
+      (epoxyResourceRoots) <<= sourceDirectory.map(src =>  Nil: Seq[File]),      
       watchedTemplates  <<= (epoxyTemplateRoots in Compile).map(etr => 
         etr.flatMap(d => (PathFinder(d) ** "*").get)
       ),
